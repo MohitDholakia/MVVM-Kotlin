58fa95a85401268efc056e561fe5ac6f9ef18235
MOHIT <ermohitsoni4@gmail.com>
2021-10-05 19:02:32 +0530

    - rebase test 22

--- app/src/main/java/com/md/demo/MyApplication.kt
+++ app/src/main/java/com/md/demo/MyApplication.kt
@@ -7,6 +7,7 @@ import org.koin.android.ext.android.startKoin
 class MyApplication : Application() {
         override fun onCreate() {
                 super.onCreate()
+                
                 startKoin(
                         this, listOf(
                                 remoteDatasourceModule,
